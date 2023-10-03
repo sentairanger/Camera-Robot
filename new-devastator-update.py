fcaeea820ec659fb10ae96d89a39f140b224eb0b
sentairanger <ed2point0@gmail.com>
2023-10-03 15:15:16 -0600

    Update new-devastator-update.py
    Made corrections

--- new-devastator-update.py
+++ new-devastator-update.py
@@ -1,14 +1,15 @@
 #import libraries
 import curses
-from gpiozero import Robot
+from gpiozero import Robot, LED
 import os
 from time import sleep
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from datetime import datetime
 
-#set variable for robot
+#set variable for robot and eye
 devastator_robot = Robot(left=(13, 21), right=(17, 27))
+eye = LED(25)
 
 # Define the new camera and the configurations
 picam2 = Picamera2()
@@ -17,19 +18,24 @@ picam2.configure(video_config)
 encoder = H264Encoder(10000000)
 moment = datetime.now()
 
+# Blink eye 2 times to ensure the code is running correctly
+eye.blink(n=2)
+
 #Define the record and stop_record functions
 def record():
     print("recording!")
-    picam2.start_recording(encoder, '/home/torvalds/Videos/video2_%02d_%02d_%02d.mjpg' % (moment.hour, moment.minute, moment.second))
+    eye.on()
+    picam2.start_recording(encoder, '/home/torvalds/Videos/video2_%02d_%02d_%02d.h264' % (moment.hour, moment.minute, moment.second))
 
 def stop_record():
+    eye.off()
     print("stop recording!")
     picam2.stop_recording()
 
 #setup your actions
 actions = {
     curses.KEY_UP:    devastator_robot.forward,
-    curses.KEY_DOWN:  devastator_robot.down,
+    curses.KEY_DOWN:  devastator_robot.backward,
     curses.KEY_LEFT:  devastator_robot.left,
     curses.KEY_RIGHT: devastator_robot.right,
 
